1869814c76a98e1606a86bde903e3d2d87b10e2a
Guilherme Oliveira <rodrigues@interativadigital.com.br>
2022-03-08 11:11:01 -0300

    add listener to buttons

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let firstValue = 0;
+let singleMathOperation = null;
+
+const btnNumbers = document.querySelectorAll('.btn-numbers');
+btnNumbers.forEach((value, key) => {
+  btnNumbers[key].addEventListener('click', function() {
+    if (singleMathOperation === null) firstValue += btnNumbers[key].value;
+  });
+});
+
+const mathOperation = document.querySelectorAll('.mathematical-operation');
+mathOperation.forEach((value, key) => {
+  mathOperation[key].addEventListener('click', function() {
+    singleMathOperation = mathOperation[key].value;
+    console.log(singleMathOperation);
+  });
+});
