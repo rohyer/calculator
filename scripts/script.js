bf824cc615ce9f2c444e76daa1c600242fa5d873
Guilherme Oliveira <rodrigues@interativadigital.com.br>
2022-03-09 21:02:45 -0300

    Develop sum, subtraction and multiplication

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,54 @@
-let firstValue = 0;
-let secondValue = 0;
-let singleMathOperation = null;
+let firstValue = 0, firstFinalValue = 0;
+let secondValue = 0, secondFinalValue = 0;
+let result = null;
+let beforeMathOperation = null;
+let inputText = document.querySelector('input[type="text"]');
 
+/**
+ * Recebe o clique do primeiro e segundo valor da conta
+ * Após o primeiro ser recebido só será recebindo o segundo valor porque o primeiro valor será ocupado pelo resultado
+ */
 const btnNumbers = document.querySelectorAll('.btn-numbers');
 btnNumbers.forEach((value, key) => {
   btnNumbers[key].addEventListener('click', function() {
-    if (singleMathOperation === null) firstValue += btnNumbers[key].value;
-    else secondValue += btnNumbers[key].value;
+    if (beforeMathOperation === null) {
+      firstValue += btnNumbers[key].value;
+      inputText.value += btnNumbers[key].value;
+    } else {
+      secondValue += btnNumbers[key].value;
+      inputText.value += btnNumbers[key].value;
+    }
   });
 });
 
+/**
+ * Recebe a operação matemática
+ * O resultado só será apresentado após o recebimento da segunda operação
+ */
 const mathOperation = document.querySelectorAll('.mathematical-operation');
 mathOperation.forEach((value, key) => {
   mathOperation[key].addEventListener('click', function() {
-    singleMathOperation = mathOperation[key].value;
+    if (!secondValue) {
+      if (firstValue) firstFinalValue = Number(firstValue);
+      beforeMathOperation = value.value;
+      inputText.value += ` ${beforeMathOperation} `;
+    } else {
+      if (secondValue) secondFinalValue = Number(secondValue);
+
+      if (beforeMathOperation === '+') result = firstFinalValue + secondFinalValue;
+      else if (beforeMathOperation === '-') result = firstFinalValue - secondFinalValue;
+      else if (beforeMathOperation === 'X') result = firstFinalValue * secondFinalValue;
+      else if (beforeMathOperation === '/') result = firstFinalValue / secondFinalValue;
+
+      beforeMathOperation = value.value;
+      inputText.value = `${result} ${beforeMathOperation} `;
+
+      console.log(`Primeiro valor: ${firstFinalValue}`);
+      console.log(`Segundo valor: ${secondFinalValue}`);
+      console.log(`Resultado: ${result}`)
+
+      secondValue = 0;
+      firstFinalValue = result;
+    }
   });
 });
